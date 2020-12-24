e827a68e89b9228c0374d6435a552286153e7895
ziloukd <leiwuyu1@163.com>
2020-12-24 19:05:36 +0800

    pytest练习

--- test_pytest/tests/test_calc.py
+++ test_pytest/tests/test_calc.py
@@ -24,11 +24,30 @@ class TestCalc:
 
     @pytest.mark.parametrize('a, b, c', [
         [1, 2, 2],
-        []
         [-1, -1, 1],
-        [1, -1, 1]
+        [1, -1, -1],
+        [0, 0, 0],
+        [2, 0, 0],
+        [1, 1/3, 1/3],
+        [1, -1/3, -1/3],
+        [1/3, 1/3, 1/9],
+        [-1/3, 1/3, -1/9],
+        [2, 0.5, 1],
+        [0.5, 0.5, 0.25],
     ])
     def test_mul(self, a, b, c):
         assert self.calc.mul(a, b) == c
-        # assert calc.mul(-1, -1) == 1
-        # assert calc.mul(1, -1) == 1
+
+    @pytest.mark.parametrize('a, b, c', [
+        [1, 2, 0.5],
+        [1, 3, 1/3],
+        [3, 2, 1.5],
+        [0, 3, 0],
+        [1/3, 3, 1/9],
+        [5, -2, -2.5],
+        [-5, -2, 2.5],
+        [0.6, 0.2, 3],
+    ])
+    def test_div(self, a, b, c):
+        assert self.calc.div(a, b) == c
+
