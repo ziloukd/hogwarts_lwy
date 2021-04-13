1e7db7ef742145e3eca65f35f4ea5565c1354607
v-leiwuyu <v-leiwuyu@xiaomi.com>
2021-04-13 15:46:26 +0800

    雪球mock实战（二）

--- mock_practice/mock_xiuqiu.py
+++ mock_practice/mock_xiuqiu.py
@@ -16,15 +16,24 @@ import json
 from mitmproxy import http, ctx
 
 
-def response(flow: http.HTTPFlow):
-    if "/batch/quote.json" in flow.request.pretty_url and "x=" in flow.request.pretty_url:
-        """
-        对一个股票维持原样；
-        对第二个股票名字加长一倍；
-        对第三个股票名字变成空"""
-        content = json.loads(flow.response.content)
-        content['data']['items'][1]['quote']['name'] = content['data']['items'][1]['quote']['name'] * 2
-        content['data']['items'][2]['quote']['name'] = ''
-        flow.response.text = json.dumps(content)
+class Rewrite:
+    def __init__(self):
+        pass
+
+    def response(self, flow: http.HTTPFlow):
+        if "/batch/quote.json" in flow.request.pretty_url and "x=" in flow.request.pretty_url:
+            """
+            对一个股票维持原样；
+            对第二个股票名字加长一倍；
+            对第三个股票名字变成空"""
+            content = json.loads(flow.response.content)
+            content['data']['items'][1]['quote']['name'] = content['data']['items'][1]['quote']['name'] * 2
+            content['data']['items'][2]['quote']['name'] = ''
+            flow.response.text = json.dumps(content)
+
+
+addons = [
+    Rewrite()
+]
 
 
